06c241fde54699106ce66a8e1005386439a19c35
Crystyano Pereira de Almeida <crystyanoalmeida@MacBook-Pro-de-Crystyano.local>
2021-01-27 19:56:43 -0300

    Updates

---
+++ precache-manifest.da230159d1ffbc5c01e0e4c8379d100c.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c8aee608f3948c89361ad486b00af60",
+    "url": "/crystyanoalmeida/index.html"
+  },
+  {
+    "revision": "3fa7ede20ead186e113c",
+    "url": "/crystyanoalmeida/static/css/main.a928f6f5.chunk.css"
+  },
+  {
+    "revision": "b91e402b8e636cfa99b0",
+    "url": "/crystyanoalmeida/static/js/2.9f6b02d5.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/crystyanoalmeida/static/js/2.9f6b02d5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fa7ede20ead186e113c",
+    "url": "/crystyanoalmeida/static/js/main.1d6785a5.chunk.js"
+  },
+  {
+    "revision": "252a32c7472e03352a53",
+    "url": "/crystyanoalmeida/static/js/runtime-main.b2234897.js"
+  },
+  {
+    "revision": "3de84b7596e91677973c456219c9a2b2",
+    "url": "/crystyanoalmeida/static/media/alien.3de84b75.svg"
+  },
+  {
+    "revision": "d1774bc8f73ae93f8340f2812a4ed911",
+    "url": "/crystyanoalmeida/static/media/android-logo.d1774bc8.svg"
+  },
+  {
+    "revision": "2d5ff1393b51b4f41d49d8e8afc14bf2",
+    "url": "/crystyanoalmeida/static/media/apple-logo.2d5ff139.svg"
+  },
+  {
+    "revision": "817c88788c6c9baedb30c81d54791c06",
+    "url": "/crystyanoalmeida/static/media/astronaut-cute.817c8878.svg"
+  },
+  {
+    "revision": "11c95b0bcfb6d7832c3079f97403d5b0",
+    "url": "/crystyanoalmeida/static/media/astronaut-holding-cup-coffe.11c95b0b.svg"
+  },
+  {
+    "revision": "27743e3d4a9c78d9dbbea8cc7477b087",
+    "url": "/crystyanoalmeida/static/media/astronaut-moon.27743e3d.svg"
+  },
+  {
+    "revision": "c0155b82f302439783ca257078757b9c",
+    "url": "/crystyanoalmeida/static/media/astronaut_listening_music_with_laptop.c0155b82.jpg"
+  },
+  {
+    "revision": "f79b35d76e41e81966609afcc2ccb235",
+    "url": "/crystyanoalmeida/static/media/cat-hug-earth.f79b35d7.svg"
+  },
+  {
+    "revision": "0a29bd988c9e4a3314985dbd84cdec24",
+    "url": "/crystyanoalmeida/static/media/csharp-logo.0a29bd98.svg"
+  },
+  {
+    "revision": "cf38b4f181fd6d7b783c25333ddad8c3",
+    "url": "/crystyanoalmeida/static/media/docker-logo.cf38b4f1.svg"
+  },
+  {
+    "revision": "1b61385e9a6272d8ecff7d1a3f5ae41c",
+    "url": "/crystyanoalmeida/static/media/earth.1b61385e.png"
+  },
+  {
+    "revision": "5eed2c8cb62e1adfae09b0129a25a1a7",
+    "url": "/crystyanoalmeida/static/media/git.5eed2c8c.svg"
+  },
+  {
+    "revision": "41557343f594a941eea88612d14cf959",
+    "url": "/crystyanoalmeida/static/media/github.41557343.svg"
+  },
+  {
+    "revision": "2dc43a8c8aa36fa3a42207f86b2f27f9",
+    "url": "/crystyanoalmeida/static/media/hacking.2dc43a8c.svg"
+  },
+  {
+    "revision": "303ef5498c727e48194b34af3479e211",
+    "url": "/crystyanoalmeida/static/media/javascript-logo.303ef549.svg"
+  },
+  {
+    "revision": "4778a1bea1424948ebf6c0d0b695ea37",
+    "url": "/crystyanoalmeida/static/media/me.4778a1be.JPEG"
+  },
+  {
+    "revision": "d54ba3e088e060bb4c27fca6807368b9",
+    "url": "/crystyanoalmeida/static/media/moon.d54ba3e0.png"
+  },
+  {
+    "revision": "20ca382f89cb50818ff2b7a82485ce36",
+    "url": "/crystyanoalmeida/static/media/planet.20ca382f.png"
+  },
+  {
+    "revision": "d3ba58adc188ff991b9b926e6ad1c4e3",
+    "url": "/crystyanoalmeida/static/media/react-logo.d3ba58ad.svg"
+  },
+  {
+    "revision": "4de6a1b806beeba741133bbd0a14a09e",
+    "url": "/crystyanoalmeida/static/media/rocket-happy.4de6a1b8.svg"
+  },
+  {
+    "revision": "2a45b66ddc7bfb9e4f01ec7ebbacb80f",
+    "url": "/crystyanoalmeida/static/media/spaceship-plane.2a45b66d.svg"
+  },
+  {
+    "revision": "abb79d6bdd079fde23c6e4ae98427e32",
+    "url": "/crystyanoalmeida/static/media/sql-server.abb79d6b.svg"
+  },
+  {
+    "revision": "d1aefbcdd88e76d0270dd815ca6a888b",
+    "url": "/crystyanoalmeida/static/media/vs19-logo.d1aefbcd.svg"
+  },
+  {
+    "revision": "661db929f24c72e56a5ea7bcdc1ad82e",
+    "url": "/crystyanoalmeida/static/media/vscode-logo.661db929.svg"
+  }
+]);
